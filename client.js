5f8869fde30cc542f2e19d6e7b1f72f965b0178a
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2020-06-22 22:54:00 -0400

    Initial commit

---
+++ client.js
@@ -0,0 +1,413 @@
+var socket = io();
+
+var USER_ID = -1;
+
+function createPrivateGame() {
+  let nameInput = document.getElementById('name-input').value;
+  socket.emit('create private lobby', nameInput);
+}
+
+socket.on('created private lobby', function(newLobby) {
+  hide('main-page');
+  USER_ID = newLobby.hostid;
+  displayLobby(newLobby);
+  console.log('Created private lobby: ', newLobby);
+});
+
+socket.on('user joined lobby', function(joinedUser) {
+  let joinedUserDiv = document.createElement('DIV');
+  joinedUserDiv.innerHTML = joinedUser.name;
+  document.getElementById('lobby-page-player-list').appendChild(joinedUserDiv);
+});
+
+function joinPrivateGame() {
+  let nameInput = document.getElementById('name-input').value;
+  let joinGameInviteCode = document.getElementById('join-private-game-input').value;
+  socket.emit('join private lobby', nameInput, joinGameInviteCode);
+}
+
+socket.on('joined user lobby', function(lobby, user) {
+  hide('main-page');
+  console.log(user);
+  USER_ID = user.id;
+  displayLobby(lobby);
+});
+
+socket.on('user left lobby', function(lobby) {
+  displayLobby(lobby);
+});
+
+function startGame() {
+  let gameSettings = {
+    DeckType: document.getElementById('deck-type-list').value,
+    TurnTime: document.getElementById('turn-timer-list').value,
+    // UsefTurn: document.getElementById('min-usef-turn').value
+  };
+
+  socket.emit('start game', USER_ID, gameSettings);
+}
+
+socket.on('game started', function(game) {
+  hide('lobby-page');
+  displayGame(game);
+});
+
+// HELPER FUNCTIONS
+
+/**
+ * Shows the html element with the specified id.
+ * @param {string} id The id of the html element to show.
+ */
+function show(id) {
+  document.getElementById(id).style.display = 'grid';
+}
+
+/**
+ * Hides the html element with the specified id.
+ * @param {string} id The id of the html element to hide.
+ */
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+const Suit = {
+  CLUBS: 1,
+  DIAMONDS: 2,
+  HEARTS: 3,
+  SPADES: 4,
+  NONE: 5
+};
+
+const SuitIcon = {
+  CLUBS: '&#9827;',
+  DIAMONDS: '&#9830;',
+  HEARTS: '&#9829;',
+  SPADES: '&#9824;',
+  NONE: '&#9996;'
+};
+
+/**
+ * Converts a suit to an icon string.
+ * @param {Suit} suit The suit to convert to an icon.
+ */
+function suitToIcon(suit) {
+  switch (suit) {
+    case Suit.CLUBS:
+      return SuitIcon.CLUBS;
+
+    case Suit.DIAMONDS:
+      return SuitIcon.DIAMONDS;
+
+    case Suit.HEARTS:
+      return SuitIcon.HEARTS;
+
+    case Suit.SPADES:
+      return SuitIcon.SPADES;
+
+    case Suit.NONE:
+      return SuitIcon.NONE;
+
+    default:
+      return 'SUIT NOT IMPLEMENTED';
+  }
+}
+
+/**
+ * Converts a suit to a string.
+ * @param {Suit} suit The suit to convert to a string.
+ */
+function suitToString(suit) {
+  switch (suit) {
+    case Suit.CLUBS:
+      return 'CLUBS';
+
+    case Suit.DIAMONDS:
+      return 'DIAMONDS';
+
+    case Suit.HEARTS:
+      return 'HEARTS';
+
+    case Suit.SPADES:
+      return 'SPADES';
+
+    case Suit.NONE:
+      return 'JOKER';
+
+    default:
+      return 'SUIT NOT IMPLEMENTED';
+  }
+}
+
+/**
+ * Converts a card to a string.
+ * @param {Card} card The card to convert to a string.
+ */
+function cardToString(card) {
+  return suitToString(card.suit) + ' ' + card.number.toString();
+}
+
+/**
+ * Returns the color of the card.
+ * @param {Card} card The card to return the color of.
+ */
+function getCardColor(card) {
+  return card.suit === Suit.CLUBS || card.suit === Suit.SPADES ? 'black' : 'red';
+}
+
+/**
+ * Returns the symbol of the card.
+ * @param {Card} card The card to get the symbol of.
+ */
+function cardNumberToSymbol(card) {
+  if (card.number === 1) return 'A';
+  else if (card.number <= 10) return card.number;
+  else if (card.number === 11) return 'J';
+  else if (card.number === 12) return 'Q';
+  else if (card.number === 13) return 'K';
+  else return 'Unkown';
+}
+
+/**
+ * Displays the specified lobby on the screen.
+ * @param {Lobby} lobby The lobby to display.
+ */
+function displayLobby(lobby) {
+  show('lobby-page');
+  document.getElementById('invite-code').innerHTML = lobby.code;
+
+  document.getElementById('lobby-page-player-list').remove();
+  let playerListDiv = document.createElement('DIV');
+  playerListDiv.id = 'lobby-page-player-list';
+  document.getElementById('lobby-page-player-list-container').appendChild(playerListDiv);
+  lobby.users.forEach((u) => {
+    let newDiv = document.createElement('DIV');
+    newDiv.innerHTML = u.name;
+    if (lobby.hostid === u.id) {
+      newDiv.style = 'border: 1px solid red;';
+    }
+    document.getElementById('lobby-page-player-list').appendChild(newDiv);
+  });
+
+  // Disable game setting fields for non-host users
+  if (USER_ID == lobby.hostid) {
+    document.getElementById('deck-type-list').disabled = false;
+    document.getElementById('turn-timer-list').disabled = false;
+    document.getElementById('min-usef-turn').disabled = false;
+    document.getElementById('start-game-button').disabled = false;
+  } else {
+    document.getElementById('deck-type-list').disabled = true;
+    document.getElementById('turn-timer-list').disabled = true;
+    document.getElementById('min-usef-turn').disabled = true;
+    document.getElementById('start-game-button').disabled = true;
+  }
+}
+
+/**
+ * Displays the players in the game page.
+ */
+function showGamePlayers(game) {
+  document.getElementById('game-page-players-list').remove();
+  let playersListDiv = document.createElement('DIV');
+  playersListDiv.id = 'game-page-players-list';
+  game.players.forEach((p) => {
+    if (p.id !== USER_ID) {
+      let playerDiv = document.createElement('DIV');
+      let playerNameDiv = document.createElement('DIV');
+      playerNameDiv.innerHTML = p.name;
+      let playerInfoDiv = document.createElement('DIV');
+      playerInfoDiv.innerHTML = '# cards: ' + p.hand.length.toString() + ', points: ' + p.points.toString();
+      playerDiv.appendChild(playerNameDiv);
+      playerDiv.appendChild(playerInfoDiv);
+      playersListDiv.appendChild(playerDiv);
+    }
+  });
+  document.getElementById('game-page-players').appendChild(playersListDiv);
+}
+
+/**
+ * Creates an html element of a card.
+ * @param {Card} card The card to create.
+ */
+function createCardElement(card) {
+  let cardElement = document.createElement('DIV');
+  cardElement.className = 'game-page-card';
+  cardElement.onclick = () => {
+    if (cardElement.className === 'game-page-card') {
+      cardElement.className = 'game-page-card game-page-card-selected';
+    } else {
+      cardElement.className = 'game-page-card';
+    }
+  }
+
+  let cardInfo = document.createElement('DIV');
+  cardInfo.className = 'card-info';
+  cardInfo.innerHTML = cardToString(card);
+  cardInfo.style.display = 'none';
+  cardElement.appendChild(cardInfo);
+
+  let cardSuit = document.createElement('DIV');
+  cardSuit.innerHTML = suitToIcon(card.suit);
+  cardSuit.style.color = getCardColor(card);
+  cardSuit.style.fontSize = 'xxx-large';
+  cardSuit.style.padding = '10px';
+  cardElement.appendChild(cardSuit);
+
+  let cardNumber = document.createElement('DIV');
+  cardNumber.innerHTML = cardNumberToSymbol(card);
+  cardNumber.style.fontSize = 'xx-large';
+  cardNumber.style.color = getCardColor(card);
+  cardElement.appendChild(cardNumber);
+
+  return cardElement;
+}
+
+/**
+ * Creates an html element of a card back.
+ */
+function createCardBackElement() {
+  let cardElement = document.createElement('DIV');
+  cardElement.className = 'game-page-card';
+  cardElement.onclick = () => {
+    if (cardElement.className === 'game-page-card') {
+      cardElement.className = 'game-page-card game-page-card-selected';
+    } else {
+      cardElement.className = 'game-page-card';
+    }
+  }
+
+  let cardInfo = document.createElement('DIV');
+  cardInfo.className = 'card-info';
+  cardInfo.innerHTML = 'DECK';
+  cardInfo.style.display = 'none';
+  cardElement.appendChild(cardInfo);
+
+  let cardBack = document.createElement('DIV');
+  cardBack.innerHTML = '&#9884;';
+  cardBack.style.fontSize = 'xxx-large';
+  cardBack.style.padding = '10px';
+  cardElement.appendChild(cardBack);
+
+  return cardElement;
+}
+
+/**
+ * Renders the deck.
+ * @param {Game} game The game to display.
+ */
+function renderDeck(game) {
+  let numCardsInDeck = game.deck.length;
+  let deckElement = document.getElementById('game-page-cards-left');
+  deckElement.style.margin = 'auto';
+  deckElement.style.padding = '10px';
+  deckElement.style.height = '70%';
+  deckElement.style.display = 'flex';
+  deckElement.style.borderRadius = '10px';
+  deckElement.style.backgroundColor = 'darkgreen';
+
+  let deckCard = createCardBackElement();
+  deckElement.appendChild(deckCard);
+
+  let deckNumCards = document.createElement('DIV');
+  deckNumCards.innerHTML = numCardsInDeck;
+  deckNumCards.style.position = 'absolute';
+  document.getElementById('game-page-deck').appendChild(deckNumCards);
+}
+
+/**
+ * Renders the discard pile.
+ * @param {Game} game The game to display.
+ */
+function renderDiscardPile(game) {
+  let discardPileTopCard = game.discardPile[game.discardPile.length - 1];
+  let discardPileElement = document.getElementById('game-page-discard-top');
+  discardPileElement.style.margin = 'auto';
+  discardPileElement.style.padding = '10px';
+  discardPileElement.style.height = '70%';
+  discardPileElement.style.display = 'flex';
+  discardPileElement.style.borderRadius = '10px';
+  discardPileElement.style.backgroundColor = 'darkgreen';
+  let cardElement = createCardElement(discardPileTopCard);
+  discardPileElement.appendChild(cardElement);
+}
+
+/**
+ * Renders the player's hand.
+ * @param {Game} game The game to display.
+ */
+function renderHand(game) {
+  document.getElementById('game-page-player-hand').remove();
+  // game-page-player-hand
+  let playerHandDiv = document.createElement('DIV');
+  playerHandDiv.id = 'game-page-player-hand';
+  let player = game.players.find((p) => p.id === USER_ID);
+  player.hand.forEach((c) => {
+    let cardDiv = createCardElement(c);
+    playerHandDiv.appendChild(cardDiv);
+  });
+  playerHandDiv.style.margin = 'auto';
+  playerHandDiv.style.padding = '10px';
+  playerHandDiv.style.height = '70%';
+  playerHandDiv.style.display = 'flex';
+  playerHandDiv.style.borderRadius = '10px';
+  playerHandDiv.style.backgroundColor = 'darkgreen';
+
+  document.getElementById('game-page-hand').appendChild(playerHandDiv);
+}
+
+/**
+ * Renders the game functions.
+ * @param {Game} game The game to display.
+ */
+function renderUsef(game) {
+  let player = null;
+  game.players.find((p) => {
+    if (p.id === game.playerTurn) {
+      player = p;
+    }
+  });
+
+  let playerTurn = player === null ? 'ERROR: Player not found.' : player.name + '\'s Turn';
+  document.getElementById('game-page-player-turn').innerHTML = playerTurn;
+}
+
+/**
+ * Displays the specified game on the screen.
+ * @param {Game} game The game to display.
+ */
+function displayGame(game) {
+  show('game-page');
+  // Render the other players in the game
+  showGamePlayers(game);
+  // Render the deck
+  renderDeck(game);
+  // Render discard pile
+  renderDiscardPile(game);
+  // Render the player hand
+  renderHand(game);
+  // Render the game functions
+  renderUsef(game);
+}
+
+/**
+ * Locks in a move.
+ */
+function lockMove() {
+  let selectedElements = document.getElementById('game-page-player-hand').getElementsByClassName('game-page-card-selected');
+  let cards = [];
+  for (let i = 0; i < selectedElements.length; i++) {
+    let cardInfo = selectedElements[i].getElementsByClassName('card-info')[0].innerHTML;
+    cards.push(cardInfo);
+  }
+  // @TODO check for discard pile selected and deck selected (only one)
+  let discardPileElement = document.getElementById('game-page-discard-top');
+  let discardTop = discardPileElement.getElementsByClassName('game-page-card-selected');
+  let discardSelected = discardTop.length > 0;
+  let deckSelected = cards.includes('DECK');
+  if (!discardSelected && !deckSelected) console.log('Select either the deck or discard to get from');
+  else if (discardSelected && deckSelected) console.log('Must select either the deck or discard, but not both');
+  else {
+    socket.emit('pick and play cards', cards, deckSelected ? 'DECK' : 'DISCARD');
+  }
+  console.log(cards);
+  console.log(discardSelected);
+}
---
+++ index.js
@@ -0,0 +1,356 @@
+var { Game, Player, Card, DeckType } = require('./game');
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/index.css', function(req, res) {
+  res.sendFile(__dirname + '/index.css');
+});
+
+app.get('/client.js', function(req, res) {
+  res.sendFile(__dirname + '/client.js');
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+class User {
+  /**
+   * @type number
+   * @description Id of the user.
+   */
+  id;
+
+  /**
+   * @type string
+   * @description Name of the user.
+   */
+  name;
+
+  /**
+   * @type number
+   * @description Id of the lobby that the user is in.
+   * Value is -1 if the user is not in a lobby.
+   */
+  lobbyid;
+
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.lobbyid = -1;
+  }
+}
+
+class Lobby {
+  /**
+   * @type number
+   * @description Id of the lobby.
+   */
+  id;
+
+  /**
+   * @type number
+   * @description Id of the host of the lobby.
+   */
+  hostid;
+
+  /**
+   * @type boolean
+   * @description True if the lobby is private.
+   */
+  locked;
+
+  /**
+   * @type string
+   * @description The invite code of the lobby.
+   */
+  code;
+
+  /**
+   * @type User[]
+   * @description List of the users in the lobby.
+   */
+  users = [];
+
+  /**
+   * @type Game
+   * @description The lobby game.
+   */
+  game = new Game();
+
+  /**
+   * @type boolean
+   * @description True if the game has started.
+   */
+  gameStarted = false;
+
+  constructor(id, host, locked, code) {
+    this.id = id;
+    this.hostid = host.id;
+    this.locked = locked;
+    this.code = code;
+    this.users.push(host);
+  }
+}
+
+/**
+ * @description All of the sockets connected to the server.
+ */
+var ALL_SOCKETS = [];
+
+/**
+ * @type Lobby[]
+ * @description List of lobbies in the server.
+ */
+var LOBBIES = [];
+
+/**
+ * @description Length of an invite code for a lobby on this server.
+ */
+const INVITE_CODE_LENGTH = 10;
+
+/**
+ * Creates an invite code string.
+ */
+function createInviteCode() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < INVITE_CODE_LENGTH; i++) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+/**
+ * Adds a new user to the server and returns it.
+ * @param {string} username Name of the user to add.
+ */
+function addUserToServer(username) {
+  let newUserId = 0;
+  ALL_SOCKETS.forEach((s) => {
+    if (s.User !== undefined && s.User.id > newUserId) newUserId = s.User.id;
+  });
+  let newUser = new User(newUserId + 1, username);
+  // console.log("Added user: ", newUser, " to the server");
+  return newUser;
+}
+
+/**
+ * Creates a new unique invitation code and returns it.
+ */
+function createUniqueInvitationCode() {
+  // Create a unique invitation code
+  let newCode = createInviteCode();
+  let validNewCode = false;
+  while(!validNewCode) {
+    validNewCode = true;
+    LOBBIES.forEach((lobby) => {
+      if (lobby.code === newCode) {
+        validNewCode = false;
+        newCode = createInviteCode();
+        return;
+      }
+    });
+  }
+  return newCode;
+}
+
+/**
+ * Adds a new lobby to the server and returns it.
+ * @param {number} host The user that is creating this lobby.
+ */
+function addLobbyToServer(host) {
+  let newLobbyId = 0;
+  LOBBIES.forEach((lobby) => {
+    if (lobby.id > newLobbyId)  newLobbyId = lobby.id;
+  });
+
+  let newCode = createUniqueInvitationCode();
+
+  let newLobby = new Lobby(newLobbyId + 1, host, true, newCode);
+  LOBBIES.push(newLobby);
+  // console.log("Added lobby: ", newLobby, " to the server");
+  return newLobby;
+}
+
+io.on('connection', function(socket) {
+  console.log('connection');
+  ALL_SOCKETS.push(socket);
+
+  socket.on('create private lobby', function(username) {
+    let newUser = addUserToServer(username);
+    socket.User = newUser;
+    let newLobby = addLobbyToServer(socket.User);
+    socket.User.lobbyid = newLobby.id;
+    socket.join(newLobby.code);
+    socket.emit('created private lobby', newLobby);
+  });
+
+  socket.on('join private lobby', function(username, joinCode) {
+    let newUser = addUserToServer(username);
+    socket.User = newUser;
+    LOBBIES.forEach((lobby) => {
+      if (lobby.code === joinCode) {
+        lobby.users.push(socket.User);
+        socket.join(joinCode);
+        socket.User.lobbyid = lobby.id;
+        socket.to(joinCode).emit('user joined lobby', socket.User);
+        socket.emit('joined user lobby', lobby, socket.User);
+      }
+    });
+  });
+
+  socket.on('start game', function(userid, gameSettings) {
+    let lobby = null;
+    LOBBIES.forEach((l) => {
+      if (l.hostid === userid) {
+        lobby = l;
+      }
+    });
+
+    if (lobby !== null) {
+      lobby.game = new Game();
+      // Set game settings
+      lobby.game.turnTime = gameSettings.TurnTime;
+      lobby.game.gameStarted = true;
+      lobby.game.numInitCards = 5;
+      lobby.game.playerTurn = userid;
+
+      lobby.game.setDeck(parseInt(gameSettings.DeckType));
+
+      // Set game players
+      lobby.users.forEach((user) => {
+        let player = new Player(user.id, user.name);
+        // Add cards to player hand
+        for (let i = 0; i < lobby.game.numInitCards; i++) {
+          player.addToHand(lobby.game.popDeck());
+        }
+
+        lobby.game.addPlayer(player);
+      });
+
+      // Set discard pile
+      lobby.game.discardCard(lobby.game.popDeck());
+
+      io.in(lobby.code).emit('game started', lobby.game);
+    }
+  });
+
+  socket.on('disconnect', function() {
+    // printUsers();
+    let removeIndex = ALL_SOCKETS.indexOf(socket);
+    if (removeIndex !== -1) ALL_SOCKETS.splice(removeIndex, 1);
+    console.log('user ', socket.User, ' disconnected');
+    // printUsers();
+
+    let removeLobby = null;
+    LOBBIES.forEach((lobby) => {
+      if (lobby.id === socket.User.lobbyid) {
+        if (lobby.hostid === socket.User.id) {
+          if (lobby.users.length <= 1) {
+            /**
+             * If the host of a lobby disconnected and
+             * there is no one else in the lobby,
+             * remove the lobby.
+             */
+            removeLobby = lobby;
+          } else {
+            /**
+             * If the host of a lobby disconnected but
+             * there are users left in the lobby,
+             * remove the host of the lobby and
+             * assign the host position to the next user in the lobby.
+             */
+            let i = lobby.users.indexOf(socket.User);
+            if (i !== -1) lobby.users.splice(i, 1);
+            lobby.hostid = lobby.users[0].id;
+            socket.to(lobby.code).emit('user left lobby', lobby);
+          }
+        } else {
+          /**
+           * If a non-host user leaves a lobby,
+           * remove them from the lobby.
+           */
+          let i = lobby.users.indexOf(socket.User);
+          if (i !== -1) lobby.users.splice(i, 1);
+          socket.to(lobby.code).emit('user left lobby', lobby);
+        }
+      }
+    });
+    if (removeLobby !== null) {
+      let i = LOBBIES.indexOf(removeLobby);
+      if (i !== -1) LOBBIES.splice(i, 1);
+    }
+  });
+
+  /* Game events */
+
+  socket.on('end turn', function() {
+    let game = new Lobby().game; // Get the appropriate lobby
+    game.nextPlayerTurn();
+  });
+
+  socket.on('pick and play cards', function(playCards, pickFrom) {
+    let lobby = new Lobby(); // Get the appropriate lobby
+    let game = lobby.game;
+    let player = game.players.find((p) => p.id === game.playerTurn);
+    if (pickFrom === 'DECK') {
+      player.addToHand(game.popDeck());
+      playerCards.forEach((card) => {
+        player.removeFromHand(card);
+        lobby.game.discardCard(card);
+      });
+    } else if (pickFrom === 'DISCARD') {
+      player.addToHand(game.popDiscardPile());
+      playerCards.forEach((card) => {
+        player.removeFromHand(card);
+        lobby.game.discardCard(card);
+      });
+    }
+  });
+
+  socket.on('call usef', function() {
+    let game = new Lobby().game; // Get the appropriate game
+    // Get the total points in hand for each player
+    // If the player that called usef has the most points/is tied, he/she loses the round
+    // Else all other players lose the round
+    let maxPoints = 0;
+    let usefPlayerPoints = 0;
+    game.players.forEach((p) => {
+      let playerPoints = p.addPointsInHand();
+      maxPoints = (playerPoints > maxPoints) ? playerPoints : maxPoints;
+      usefPlayerPoints = (p.id === game.playerTurn) ? playerPoints : usefPlayerPoints;
+    });
+
+    if (usefPlayerPoints < maxPoints) {
+      // Usef player wins round
+      lobby.game.players.forEach((p) => {
+        if (p.id !== game.playerTurn) {
+          p.addPoints(p.addPointsInHand());
+        }
+        // @todo check if score is above the score limit
+      });
+    } else {
+      // Other players win round
+      let losingPlayer = game.players.find((p) => p.id === game.playerTurn);
+      losingPlayer.addPoints(losingPlayer.addPointsInHand());
+      // @todo check if score is above the score limit
+    }
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
+
+// Testing
+function printUsers() {
+  console.log('Printing users: ');
+  ALL_SOCKETS.forEach((s) => {
+    console.log(s.User);
+  });
+  console.log('----------');
+}
